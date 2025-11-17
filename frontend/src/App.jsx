fedeced590478ef05b7bcb94840ab687a9d1e7d8
Saurabh <sauravthakulla683@gmail.com>
2025-11-17 22:39:30 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+       <h1>Feed</h1>
+       <h1>Profile</h1>
+    </div>
+  );
+};
+
+export default App;

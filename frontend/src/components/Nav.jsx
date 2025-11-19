2cb95bb6aca767edeae0bbfe4a5e585ec0510ac7
Saurabh <sauravthakulla683@gmail.com>
2025-11-20 01:14:09 +0530

    Nav bar updated

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const Nav = () => {
   return (
-    <div className="flex justify-between  w-full h-[3rem] bg-blue-400 flex-row">
-      <div>Social_APP</div>
-      <div className="flex w-1/3 flex-row justify-around items-center text-xl p-3">
-        <li className="list-none">Feed</li>
-        <li className="list-none">Profile</li>
+    <div className="flex justify-around font-inter text-white w-full h-[4rem] bg-black flex-row">
+      <div className="text-2xl p-4 ">Social_APP</div>
+      <div className="flex w-1/2 flex-row justify-around items-center text-xl p-3 max-md:justify-between">
+        <li className="list-none mr-[6rem] max-md:mr-0">Feed</li>
+        <li className="list-none ml-[10rem] max-md:hidden">People</li>
+        <button className="md:hidden w-10 h-7 bg-white"></button>
       </div>
     </div>
   );

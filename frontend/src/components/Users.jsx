72efdb129460caddc68b34962be9ef2266f2fc72
Saurabh <sauravthakulla683@gmail.com>
2026-01-08 23:26:07 +0530

    EDIT : Code Structure Changed

--- frontend/src/components/Users.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Users = () => {
-  return (
-    <div className="max-md:hidden w-[40%] bg-blue-400  h-screen border-2 border-black"></div>
-  );
-};
-
-export default Users;

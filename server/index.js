cda1c6f6fcdd04b9e88e93d8a4f5cf863350bcc6
Madan k <madanyadav8748810120@gmail.com>
2026-01-27 23:11:26 +0530

    add: index

---
+++ server/index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const userRoutes = require('./routes/userRoutes');
+const postRoutes = require('./routes/postRoutes');

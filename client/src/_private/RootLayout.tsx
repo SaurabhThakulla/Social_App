3a5efe94a37eae4808e1c45295472ce31a3c3f9f
Saurabh <sauravthakulla683@gmail.com>
2026-02-07 22:59:58 +0530

    fix : bug

--- client/src/_private/RootLayout.tsx
+++ client/src/_private/RootLayout.tsx
@@ -1,17 +1,17 @@
 import Navbar from "@/components/shared/Navbar";
-import { Home, Profile, Stories  } from "./pages";
 import AppSidebar from "@/components/shared/AppSidebar";
 import { usePosts } from "@/api/queries";
+import { Outlet } from "react-router-dom";
+import { Stories } from "./pages";
 
 const RootLayout = () => {
-  const profile = true;
   const { data: posts } = usePosts();
 
   return(
     <div className="grid grid-cols-[0.3fr_2fr]  md:grid-cols-[0.5fr_3.4fr_1fr] min-h-screen">
       <AppSidebar />
       <Navbar>
-        {!profile ? <Home /> : <Profile />}
+        <Outlet/>
      </Navbar>
       <Stories posts={posts || []} />
     </div>

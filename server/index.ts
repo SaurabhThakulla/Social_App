b039d5ce0f238dbf7772f7155bb8fffd5751e5db
Saurabh <sauravthakulla683@gmail.com>
2026-02-28 23:57:10 +0530

    add : auth setup server side

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,25 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import routes from "./routes/routes";
+import { connectDB } from "./config/db";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.listen(3000, () => {
-  console.log("Server running on port 5000");
-});
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", routes);
+
+const startServer = async function () {
+  await connectDB();
+
+  app.listen(PORT, function () {
+    console.log("🚀 Server running on port " + PORT);
+  });
+};
+
+startServer();

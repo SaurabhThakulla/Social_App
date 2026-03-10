f2d976d8524bebba870ff211b84b48dc3df646e1
Saurabh <sauravthakulla683@gmail.com>
2026-03-10 22:32:22 +0530

    add : post route api

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRoutes from "../modules/auth/auth.routes";
+import postRoutes from "../modules/posts/post.routes";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/posts", postRoutes);
 
 export default router;
